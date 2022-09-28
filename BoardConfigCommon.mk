dotOS-Devices/device_lenovo_jd2019-common

sdm710-common: override sdm845 hals

BoardConfigCommon.mk
@@ -89,6 +89,9 @@ TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x200000U | 0x2000U
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
+# Override hals
+OVERRIDE_QCOM_HARDWARE_VARIANT := sdm845-pe
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824

vendorsetup.sh
@@ -1,0 +1,2 @@
+cp hardware/qcom-caf/sdm845/Android.bp hardware/qcom-caf/sdm845-pe/
+cp hardware/qcom-caf/sdm845/Android.mk hardware/qcom-caf/sdm845-pe/

HsyHusker/device_lenovo_sdm710-common-tiramisu

BoardConfigCommon.mk
@@ -85,6 +85,9 @@ BOARD_HAS_QCA_FM_SOC := "cherokee"
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
+# Override hals
+OVERRIDE_QCOM_HARDWARE_VARIANT := sdm845-lenovo
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824

vendorsetup.sh
@@ -1,0 +1,2 @@
+cp hardware/qcom-caf/sdm845/Android.bp hardware/qcom-caf/sdm845-lenovo/
+cp hardware/qcom-caf/sdm845/Android.mk hardware/qcom-caf/sdm845-lenovo/